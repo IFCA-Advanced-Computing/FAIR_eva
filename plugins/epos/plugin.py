68528e88a8d4aa1cebe26bafb7ff47df325e995f
Pablo Orviz <orviz@ifca.unican.es>
2024-10-18 10:57:08 +0200

    Update class docstring

--- plugins/epos/plugin.py
+++ plugins/epos/plugin.py
@@ -26,22 +26,7 @@ logger = logging.getLogger("api.plugin")
 
 
 class Plugin(EvaluatorBase):
-    """A class used to define FAIR indicators tests. It is tailored towards the EPOS repository
-
-    ...
-
-    Attributes
-    ----------
-    item_id : str
-        Digital Object identifier, which can be a generic one (DOI, PID), or an internal (e.g. an
-            identifier from the repo)
-
-    oai_base : str
-        Open Archives Initiative , This is the place in which the API will ask for the metadata. If you are working with  EPOS https://www.ics-c.epos-eu.org/api/v1/resources
-
-    lang : Language
-
-    """
+    """A class used to define FAIR indicators tests, tailored to the DT-GEO prototype metadata catalog (EPOS ICS-C based)."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(
